c7635bacc61e69acd992da31d6b1e5d9f67b8575
Larisa Badmaeva <larilotus12@gmail.com>
2023-06-19 18:23:53 +0300

    add test

--- test_reqres.py
+++ test_reqres.py
@@ -7,34 +7,44 @@ from helper import load_json_schema, CustomSession, reqres_session
 
 def test_requested_page_number():
     page = 2
+    schema = load_json_schema('get_requested_page_number.json')
 
     response = reqres_session.get('/api/users', params={'page': page})
+
+    validate(instance=response.json(), schema=schema)
     assert response.status_code == 200
     assert response.json()['page'] == page
 
 
 def test_users_list_default_length():
     default_users_count = 6
+    schema = load_json_schema('get_user_list.json')
 
     response = reqres_session.get('/api/users')
 
+    validate(instance=response.json(), schema=schema)
     assert len(response.json()['data']) == default_users_count
 
 
 def test_single_user():
     first_name = "Janet"
     last_name = "Weaver"
+    schema = load_json_schema('get_single_user.json')
 
-    response = requests.get('https://reqres.in/api/users/2')
+    response = reqres_session.get('/api/users/2')
 
+    validate(instance=response.json(), schema=schema)
     assert response.status_code == 200
     assert response.json()['data']['first_name'] == first_name
     assert response.json()['data']['last_name'] == last_name
 
 
 def test_single_user_not_found():
-    response = requests.get('https://reqres.in/api/users/23')
+    schema = load_json_schema('get_single_user_not_found.json')
 
+    response = reqres_session.get('/api/users/23')
+
+    validate(instance=response.json(), schema=schema)
     assert response.status_code == 404
     assert response.text == '{}'
 
@@ -59,6 +69,7 @@ def test_create_user():
 def test_post_register_successful():
     email = "eve.holt@reqres.in",
     password = "pistol"
+    schema = load_json_schema('post_register_successful.json')
 
     response = requests.post(
         url='https://reqres.in/api/register',
@@ -67,17 +78,20 @@ def test_post_register_successful():
             'password': password}
     )
 
+    validate(instance=response.json(), schema=schema)
     assert response.status_code == 400
 
 
 def test_post_register_unsuccessful():
     email = "sydney@fife"
+    schema = load_json_schema('post_register_unsuccessful.json')
 
     response = requests.post(
         url='https://reqres.in/api/register',
         json={'email': email}
     )
 
+    validate(instance=response.json(), schema=schema)
     assert response.status_code == 400
     assert response.text == '{"error":"Missing password"}'
 
@@ -85,6 +99,7 @@ def test_post_register_unsuccessful():
 def test_login_successful():
     email = 'eve.holt@reqres.in'
     password = 'cityslicka'
+    schema = load_json_schema('post_login_successful.json')
 
     response = requests.post(
         url='https://reqres.in/api/login',
@@ -92,17 +107,19 @@ def test_login_successful():
               'password': password}
     )
 
+    validate(instance=response.json(), schema=schema)
     assert response.status_code == 200
 
 
 def test_login_unsuccessful():
     email = 'peter@klaven'
+    schema = load_json_schema('post_login_unsuccessful.json')
 
     response = requests.post(
         url='https://reqres.in/api/login',
         json={'email': email}
     )
-
+    validate(instance=response.json(), schema=schema)
     assert response.status_code == 400
     assert response.text == '{"error":"Missing password"}'
 
@@ -110,6 +127,7 @@ def test_login_unsuccessful():
 def test_put_user():
     name = "morpheus"
     job = "zion resident"
+    schema = load_json_schema('put_user.json')
 
     response = requests.put(
         url='https://reqres.in/api/users/2',
@@ -119,13 +137,17 @@ def test_put_user():
         }
     )
 
+    validate(instance=response.json(), schema=schema)
     assert response.status_code == 200
     assert response.json()['name'] == name
     assert response.json()['job'] == job
 
 
 def test_delete_user_returns_204():
-    response = requests.delete(url='https://reqres.in/api/users/2')
+    # schema = load_json_schema('get_delete_user_returns.json')
 
-    assert response.status_code == 204
+    response = reqres_session.get('/api/users/2')
+    print(response.text)
+    # validate(instance=response.json(), schema=schema)
+    assert response.status_code == 200
     assert response.text == ''
